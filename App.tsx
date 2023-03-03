eb3dc58203ab6668aeff55dcae864b7059de51e7
Felipe Sanfilippo <feliperodrigues.san@gmail.com>
2023-03-03 09:57:04 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Platform, Button } from 'react-native'
+import {
+  useAuthRequest,
+  makeRedirectUri,
+  AuthRequestConfig,
+  DiscoveryDocument
+} from 'expo-auth-session'
+
+import { maybeCompleteAuthSession } from 'expo-web-browser';
+import Constants from 'expo-constants';
+
+
+
 
 export default function App() {
+
+maybeCompleteAuthSession();
+
+const useProxy = Constants.appOwnership === 'expo' && false;
+
+
+
+const discovery: DiscoveryDocument = {
+  authorizationEndpoint: 'https://www.facebook.com/v6.0/dialog/oauth',
+  tokenEndpoint: 'https://graph.facebook.com/v6.0/oauth/access_token'
+}
+
+const config: AuthRequestConfig = {
+  clientId: '910491159993698',
+  scopes: ['public_profile', 'user_likes'],
+  redirectUri: makeRedirectUri({
+    native: 'fb910491159993698://authorize', 
+    useProxy
+  }),
+  extraParams: {
+    display: Platform.select({ web: 'popup' })!
+  }
+}
+
+  const [request, response, promptAsync] = useAuthRequest(config, discovery)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button onPress={() => promptAsync( {useProxy} )} title="Login" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +52,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
